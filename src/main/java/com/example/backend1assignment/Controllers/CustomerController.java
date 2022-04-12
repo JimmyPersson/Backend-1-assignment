b1881e9a6569c97bdd706cddf697ef4a1c50cec8
Jimmy Persson <jimmy.persson85@gmail.com>
2022-04-12 12:04:08 +0200

    Added "add function" in CustomerController.java

--- src/main/java/com/example/backend1assignment/Controllers/CustomerController.java
+++ src/main/java/com/example/backend1assignment/Controllers/CustomerController.java
@@ -1,8 +1,10 @@
 package com.example.backend1assignment.Controllers;
 
+import com.example.backend1assignment.Models.Customer;
 import com.example.backend1assignment.Repos.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,18 @@ public class CustomerController {
 
     @Autowired
     private CustomerRepository customerRepository;
+
+    @RequestMapping("/add")
+    public String addCustomer(@RequestParam String name, @RequestParam String address,
+                              @RequestParam String email, @RequestParam (required = false) String password){
+        Customer customer = new Customer();
+        customer.setName(name);
+        customer.setAddress(address);
+        customer.setEmail(email);
+        customer.setPassword(password);
+
+        customerRepository.save(customer);
+
+        return "Customer with name " + name + " has been saved!";
+    }
 }
