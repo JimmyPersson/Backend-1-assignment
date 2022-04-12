c738d4f808ba95203699d2af60fd29e48e54ebbe
Jimmy Persson <jimmy.persson85@gmail.com>
2022-04-12 10:32:41 +0200

    The big bang

---
+++ src/test/java/com/example/backend1assignment/Backend1AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend1assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend1AssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
