bbcd87b99e11fcb7603564c1ebb27d3f951d3251
Jimmy Persson <jimmy.persson85@gmail.com>
2022-04-12 17:28:43 +0200

    Added CustomerDTO

--- src/main/java/com/example/backend1assignment/Controllers/CustomerController.java
+++ src/main/java/com/example/backend1assignment/Controllers/CustomerController.java
@@ -1,30 +1,30 @@
 package com.example.backend1assignment.Controllers;
 
 import com.example.backend1assignment.Models.Customer;
+import com.example.backend1assignment.Models.DTO.CustomerDTO;
 import com.example.backend1assignment.Repos.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/customers")
 public class CustomerController {
-//new branch
+
     @Autowired
     private CustomerRepository customerRepository;
 
-    @RequestMapping("/add")
-    public String addCustomer(@RequestParam String name, @RequestParam String address,
-                              @RequestParam String email, @RequestParam (required = false) String password){
+    @PostMapping("/add")
+    public ResponseEntity<?> addCustomer(@RequestBody CustomerDTO customerDTO){
         Customer customer = new Customer();
-        customer.setName(name);
-        customer.setAddress(address);
-        customer.setEmail(email);
-        customer.setPassword(password);
+        customer.setName(customerDTO.getName());
+        customer.setAddress(customerDTO.getAddress());
+        customer.setEmail(customerDTO.getEmail());
+        customer.setPassword(customerDTO.getPassword());
 
         customerRepository.save(customer);
 
-        return "Customer with name " + name + " has been saved!";
+        return new ResponseEntity<>(customerDTO, HttpStatus.CREATED);
     }
 }
