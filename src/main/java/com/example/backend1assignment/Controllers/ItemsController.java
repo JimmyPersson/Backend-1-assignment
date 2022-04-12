c05cfb2fee127de30a1e35e9971675181037f98a
xantippas <xantippa.hogberg@gmail.com>
2022-04-12 18:32:44 +0200

    added query method to itemscontroller class to find item by name and product number. Added method in itemscontroller class to buy an item

--- src/main/java/com/example/backend1assignment/Controllers/ItemsController.java
+++ src/main/java/com/example/backend1assignment/Controllers/ItemsController.java
@@ -1,11 +1,18 @@
 package com.example.backend1assignment.Controllers;
 
+import com.example.backend1assignment.Models.BuyOrders;
+import com.example.backend1assignment.Models.Customer;
 import com.example.backend1assignment.Models.Items;
+import com.example.backend1assignment.Repos.BuyOrdersRepository;
+import com.example.backend1assignment.Repos.CustomerRepository;
 import com.example.backend1assignment.Repos.ItemsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/items")
@@ -13,6 +20,12 @@ public class ItemsController {
 
     @Autowired
     ItemsRepository itemsRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private BuyOrdersRepository buyOrdersRepository;
 
     @GetMapping("/all")
     public Iterable<Items> getAllItems(){
@@ -31,6 +44,24 @@ public class ItemsController {
         itemsRepository.save(item);
 
         return new ResponseEntity<>(item, HttpStatus.CREATED);
+    }
+
+    @RequestMapping("/buy")
+    public ResponseEntity<?> newOrder(@RequestParam Long customerId, @RequestParam String itemName, @RequestParam String productNumber){
+        Customer customer = customerRepository.findById(customerId).get();
+        Items item = itemsRepository.findByNameAndProductNumber(itemName, productNumber);
+
+        List<Items> myOrder = new ArrayList<>();
+        myOrder.add(item);
+
+        BuyOrders buyOrders = new BuyOrders();
+        //buyOrders.setOrderNumber(orderNumber);
+        buyOrders.setCustomer(customer);
+        buyOrders.setItems(myOrder);
+
+        buyOrdersRepository.save(buyOrders);
+
+        return new ResponseEntity<>(buyOrders, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/com/example/backend1assignment/Repos/ItemsRepository.java
+++ src/main/java/com/example/backend1assignment/Repos/ItemsRepository.java
@@ -3,5 +3,9 @@ package com.example.backend1assignment.Repos;
 import com.example.backend1assignment.Models.Items;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface ItemsRepository extends CrudRepository<Items, Long> {
+
+    Items findByNameAndProductNumber(String name, String productNumber);
 }
