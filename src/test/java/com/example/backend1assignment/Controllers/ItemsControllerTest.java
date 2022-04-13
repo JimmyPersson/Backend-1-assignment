587d24557f04ce21e14ba487e9f409ddee57bed2
xantippas <xantippa.hogberg@gmail.com>
2022-04-13 10:11:49 +0200

    generated test classes for buyorder and items controller class

---
+++ src/test/java/com/example/backend1assignment/Controllers/ItemsControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.backend1assignment.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ItemsControllerTest {
+
+    @Test
+    void getAllItems() {
+    }
+
+    @Test
+    void getItemById() {
+    }
+
+    @Test
+    void addItem() {
+    }
+
+    @Test
+    void newOrder() {
+    }
+}
