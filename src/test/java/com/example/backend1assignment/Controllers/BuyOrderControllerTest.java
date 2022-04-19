93a8e34e27ba2a0c2e98b0e87cac5285c7a25b40
xantippas <xantippa.hogberg@gmail.com>
2022-04-19 19:18:50 +0200

    added test method functionality for get all orders

--- src/test/java/com/example/backend1assignment/Controllers/BuyOrderControllerTest.java
+++ src/test/java/com/example/backend1assignment/Controllers/BuyOrderControllerTest.java
@@ -2,13 +2,10 @@ package com.example.backend1assignment.Controllers;
 
 import com.example.backend1assignment.Models.BuyOrders;
 import com.example.backend1assignment.Models.Customer;
-import com.example.backend1assignment.Models.DTO.BuyOrderDTO;
 import com.example.backend1assignment.Models.Items;
 import com.example.backend1assignment.Repos.BuyOrdersRepository;
 import com.example.backend1assignment.Repos.CustomerRepository;
-import com.example.backend1assignment.Repos.ItemsRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -41,13 +38,9 @@ class BuyOrderControllerTest {
     @MockBean
     private CustomerRepository customerMockRepository;
 
-    @MockBean
-    private ItemsRepository itemMockRepository;
-
-    @BeforeEach
-    public void init() {
-
-        Customer c1 = new Customer(2L, "Bob", "Bobs address", "bob@email.com", "secret2");
+    @Test
+    void getAllOrders() throws Exception {
+        Customer c1 = new Customer(1L, "Bob", "Bobs address", "bob@email.com", "secret2");
         Items i1 = new Items(1L, "Toothbrush", "HY01");
         List<Items> l1 = new ArrayList<>();
         l1.add(i1);
@@ -55,39 +48,37 @@ class BuyOrderControllerTest {
         BuyOrders b1 = new BuyOrders(1L, "3E", c1, l1);
         BuyOrders b2 = new BuyOrders(2L, "2E", c1, l1);
         BuyOrders b3 = new BuyOrders(3L, "4E", c1, l1);
+        List<BuyOrders> allOrders = new ArrayList<>();
+        allOrders.add(b1);
+        allOrders.add(b2);
+        allOrders.add(b3);
+        c1.setOrders(allOrders);
 
-        when(mockRepository.findById(1L)).thenReturn(Optional.of(b1));
         when(mockRepository.findAll()).thenReturn(Arrays.asList(b1, b2, b3));
-        when(customerMockRepository.findById(2L)).thenReturn(Optional.of(c1));
-    }
 
-    @Test
-    void getAllOrders() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/orders").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[{\"id\":1,\"name\":\"Anna\",\"address\":\"Anna address\",\"email\":\"anna@email.com\",\"password\":\"secret1\"}," +
-                        "{\"id\":2,\"name\":\"Bob\",\"address\":\"Bobs address\",\"email\":\"bob@email.com\",\"password\":\"secret2\"},"+
-                        "{\"id\":3,\"name\":\"Cecile\",\"address\":\"Ceciles address\",\"email\":\"cecile@email.com\",\"password\":\"secret3\"}]"));
+                .andExpect(content().json(asJsonString(allOrders))).andDo(MockMvcResultHandlers.print());
 
     }
 
     @Test
     void getOrderByCustomer() throws Exception {
-        Customer cus = new Customer(7L, "A", "A", "A", "A");
-        Items pres = new Items(7L, "A", "A");
-        List<Items> myIt = new ArrayList<>();
-        myIt.add(pres);
-        BuyOrders bo = new BuyOrders(7L, "33", cus, myIt);
-        List<BuyOrders> myOr = new ArrayList<>();
-        myOr.add(bo);
-        cus.setOrders(myOr);
+        Customer c2 = new Customer(2L, "Karl", "Karls gata", "kg@email.com", "secretpassword");
+        Items i2 = new Items(2L, "Pillow", "CO01");
+        List<Items> items = new ArrayList<>();
+        items.add(i2);
+        BuyOrders bo = new BuyOrders(1L, "5E", c2, items);
+        List<BuyOrders> orderedItems = new ArrayList<>();
+        orderedItems.add(bo);
+        c2.setOrders(orderedItems);
 
-        when(customerMockRepository.findById(7L)).thenReturn(Optional.of(cus));
+        when(customerMockRepository.findById(2L)).thenReturn(Optional.of(c2));
         when(mockRepository.findAll()).thenReturn(Arrays.asList(bo));
 
-        mvc.perform(MockMvcRequestBuilders.get("/orders/7")
+        mvc.perform(MockMvcRequestBuilders.get("/orders/2")
                         .accept(MediaType.APPLICATION_JSON).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk()).andExpect(content().json(asJsonString(myOr))).andDo(MockMvcResultHandlers.print());
+                .andExpect(status().isOk()).andExpect(content().json(asJsonString(orderedItems))).andDo(MockMvcResultHandlers.print());
     }
 
     public static String asJsonString(final Object obj) {
